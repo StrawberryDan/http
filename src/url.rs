04cc1f41a9e1fe7a74600eea7f05f790373c3022
daniel <DanRhysMorris@gmail.com>
2022-03-13 20:40:41 +0000

    Added support for full URL encoding and decoding

--- src/url.rs
+++ src/url.rs
@@ -1,51 +1,168 @@
+use std::borrow::Borrow;
+use std::fmt::Display;
 use crate::Error;
 
 #[derive(Debug)]
 pub struct URL {
-    protocol: String,
-    username: String,
-    password: String,
-    host: String,
+    protocol: Option<String>,
+    username: Option<String>,
+    password: Option<String>,
+    host: Option<String>,
+    port: Option<u16>,
     resource: String,
 }
 
 impl URL {
+    pub fn new() -> Self {
+        Self {
+            protocol: None,
+            username: None,
+            password: None,
+            host: None,
+            port: None,
+            resource: "/".to_string()
+        }
+    }
+
     pub fn from_string(s: &str) -> Result<URL, Error> {
-        let (protocol, s) = s.split_once("://").unwrap_or(("", s));
+        if !s.is_ascii() || s.is_empty() { return Err(Error::URLParse); }
+
+        let mut s = s.to_string();
+        while let Some(i) = s.find("%") {
+            let (prefix, middle, suffix) = (
+                (0..i).map(|n| s.chars().nth(n).unwrap()).collect::<String>(),
+                (i+1..i+3).map(|n| s.chars().nth(n).unwrap()).collect::<String>(),
+                (i+3..s.len()).map(|n| s.chars().nth(n).unwrap()).collect::<String>()
+            );
+
+            let decoded = u8::from_str_radix(&middle, 16).map_err(|_| Error::URLParse)? as char;
+            s = format!("{}{}{}", prefix, decoded, suffix);
+        }
+
+        let (protocol, s) = s.split_once("://").unwrap_or(("", &s));
         let (user, s) = s.split_once("@").unwrap_or(("", s));
         let (host, resource) = s.split_once("/").ok_or(Error::URLParse)?;
+        let (host, port) = host.split_once(":").unwrap_or((host, ""));
         let (username, password) = user.split_once(":").unwrap_or(("", ""));
+
         Ok( Self {
-            protocol: protocol.to_string(),
-            username: username.to_string(),
-            password: password.to_string(),
-            host: host.to_string(),
+            protocol: if protocol.is_empty() { None } else { Some(protocol.to_string()) },
+            username: if username.is_empty() { None } else { Some(user    .to_string()) },
+            password: if password.is_empty() { None } else { Some(password.to_string()) },
+            host:     if host    .is_empty() { None } else { Some(host    .to_string()) },
+            port:     if port    .is_empty() { None } else { Some(port    .parse().map_err(|_| Error::URLParse)? ) },
             resource: format!("/{}", resource)
         } )
     }
 
-    pub fn protocol(&self) -> &String {
-        &self.protocol
+    pub fn protocol(&self) -> Option<&String> {
+        self.protocol.as_ref()
     }
 
-    pub fn username(&self) -> &String {
-        &self.username
+    pub fn with_protocol<S: Borrow<str>>(self, protocol: Option<S>) -> Self {
+        Self { protocol: protocol.map( |s| s.borrow().to_string() ), .. self }
     }
 
-    pub fn password(&self) -> &String {
-        &self.password
+    pub fn username(&self) -> Option<&String> {
+        self.username.as_ref()
     }
 
-    pub fn host(&self) -> &String {
-        &self.host
+    pub fn with_username<S: Borrow<str>>(self, username: Option<S>) -> Self {
+        Self { username: username.map( |s| s.borrow().to_string() ), .. self }
+    }
+
+    pub fn password(&self) -> Option<&String> {
+        self.password.as_ref()
+    }
+
+    pub fn with_password<S: Borrow<str>>(self, password: Option<S>) -> Self {
+        Self { password: password.map( |s| s.borrow().to_string() ), .. self }
+    }
+
+    pub fn host(&self) -> Option<&String> {
+        self.host.as_ref()
+    }
+
+    pub fn with_host<S: Borrow<str>>(self, host: Option<S>) -> Self {
+        Self { host: host.map( |s| s.borrow().to_string() ), .. self }
+    }
+
+    pub fn port(&self) -> Option<&u16> {
+        self.port.as_ref()
+    }
+
+    pub fn with_port(self, port: Option<u16>) -> Self {
+        Self { port, .. self }
     }
 
     pub fn resource(&self) -> &String {
         &self.resource
     }
+
+    pub fn with_resource<S: Borrow<str>>(self, resource: S) -> Self {
+        let resource = resource.borrow();
+        if resource.starts_with("/") {
+            Self { resource: resource.to_string(), .. self }
+        } else {
+            Self { resource: format!("/{}", resource), .. self }
+        }
+    }
+
+    pub fn as_string(&self) -> Result<String, Error> {
+        let mut s = String::new();
+
+        if let Some(p) = &self.protocol {
+            s = format!("{}://", encode(p));
+        }
+
+        if let (Some(user), Some(pass)) = (&self.username, &self.password) {
+            s += &format!("{}:{}", encode(user), encode(pass));
+        }
+
+        if let Some(host) = &self.host {
+            s += &encode(host);
+        }
+
+        if let Some(port) = self.port {
+            s += &format!(":{}", port);
+        }
+
+        s += &encode(&self.resource);
+
+        if s.is_ascii() {
+            Ok(s)
+        } else {
+            Err(Error::URLParse)
+        }
+    }
 }
 
-impl URL {
-
+fn encode<S: Borrow<str>>(s: &S) -> String {
+    s.borrow().chars()
+        .map(|c| match c {
+                'a' ..= 'z' | 'A' ..= 'Z' | '0' ..= '9' | '-' | '_' | '~' | '.' => format!{"{}", c},
+                _ => format!("%{:02X?}", (c as u8))
+            })
+        .fold(String::new(), |a, b| format!("{}{}", a, b))
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn url_encoding() {
+        let url = URL::new()
+            .with_protocol(Some("https"))
+            .with_host(Some("justnoise.net"))
+            .with_port(Some(25565))
+            .with_resource("ch{ungus}/monkey picture.png");
+
+        println!("{}", url.as_string().unwrap());
+    }
+
+    #[test]
+    fn url_decoding() {
+        println!("{:?}", URL::from_string("https://justnoise.net:25565/el%20diablo/the%20devil.png").unwrap());
+    }
+}
