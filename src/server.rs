2547a88fa518d3b5a0a2d90a9dd238fa1d54e71c
daniel <DanRhysMorris@gmail.com>
2022-03-29 23:11:37 +0100

    Added with_key and with_certificate to WebService

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use std::io::{Read, Write};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 use crate::thread_pool::ThreadPool;
@@ -7,6 +8,8 @@ use crate::thread_pool::ThreadPool;
 pub struct Server<H: WebService + Send + Sync + 'static> {
     socket: SocketAddr,
     handler: Arc<H>,
+    certificate: Option<PathBuf>,
+    key: Option<PathBuf>,
 }
 
 impl<H: WebService + Send + Sync + 'static> Server<H> {
@@ -14,7 +17,13 @@ impl<H: WebService + Send + Sync + 'static> Server<H> {
         Self {
             socket: SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8080),
             handler: Arc::new(handler),
+            certificate: None,
+            key: None,
         }
+    }
+
+    pub fn with_socket(self, socket: SocketAddr) -> Self {
+        Self { socket, ..self }
     }
 
     pub fn with_address(self, addr: IpAddr) -> Self {
@@ -27,8 +36,12 @@ impl<H: WebService + Send + Sync + 'static> Server<H> {
         Self { socket, .. self }
     }
 
-    pub fn with_socket(self, socket: SocketAddr) -> Self {
-        Self { socket, ..self }
+    pub fn with_certificate<P: AsRef<Path>>(self, certificate: Option<P>) -> Self {
+        Self { certificate: certificate.map(|x| x.as_ref().to_path_buf()), .. self }
+    }
+
+    pub fn with_key<P: AsRef<Path>>(self, key: Option<P>) -> Self {
+        Self { key: key.map(|x| x.as_ref().to_path_buf()), .. self }
     }
 
     pub fn run(&mut self) {
@@ -59,9 +72,9 @@ impl<H: WebService + Send + Sync + 'static> Server<H> {
 
         let tls = {
             let mut tls = openssl::ssl::SslAcceptor::mozilla_intermediate_v5(openssl::ssl::SslMethod::tls()).unwrap();
-            tls.set_certificate_file("cert.pem", openssl::ssl::SslFiletype::PEM)
+            tls.set_certificate_file(self.certificate.as_ref().unwrap(), openssl::ssl::SslFiletype::PEM)
                 .expect("Expected certificate file \"cert.pem\" in working directory!");
-            tls.set_private_key_file("key.pem", openssl::ssl::SslFiletype::PEM)
+            tls.set_private_key_file(self.key.as_ref().unwrap(), openssl::ssl::SslFiletype::PEM)
                 .expect("Expected key file \"key.pem\" in working directory!");
             tls.build()
         };
